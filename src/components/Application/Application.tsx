17114f04d17e8036f5cf2b09b094b302e7e35300
Dato Abramishvili <abramishvili98@gmail.com>
2024-05-08 20:11:10 +0400

    first commit

---
+++ src/components/Application/Application.tsx
@@ -0,0 +1,11 @@
+import styles from './Application.module.css';
+
+const Application = () => {
+  return (
+    <main className={styles.Wrapper}>
+      <h1>Hello World</h1>
+    </main>
+  );
+};
+
+export default Application;
